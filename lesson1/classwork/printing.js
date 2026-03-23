59ae9a87d1f3a4568efa285ed4d73d5c1e69de1f
LayaRaghuGH <layaraghu14@gmail.com>
2026-03-23 00:58:13 +0000

    Finished lesson 1

---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+console.log("My name is Laya.");
+console.log('My favourite food is Burger');
+
+// Error: quotation marks mismatched
+// console.log('This is a comment");
---
+++ lesson1/classwork/variables.js
@@ -0,0 +1,8 @@
+let x = 5;  // number: whole-number value
+console.log(x);
+
+let number = 3.14;  // number: decimal value
+console.log(number);
+
+let favourite_food = "burger"; // string: text
+console.log("My favourite food is " + favourite_food); // string concatenation
