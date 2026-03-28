786b082ef9b34a99524767a67148250706873a35
LayaRaghuGH <layaraghu14@gmail.com>
2026-03-28 18:49:44 +0000

    completed lesson 1 homework

--- lesson1/homework/problems.js
+++ lesson1/homework/problems.js
@@ -1,24 +1,37 @@
 // Problem 1
 // Create a variable for your lucky number and print it.
-
+let luckynumber = 9; // number: whole-number value
+console.log("My lucky number is " + luckynumber);
 
 
 // Problem 2
 // Create a variable for the number of hours you sleep and print it.
-
+let hours = 9.5; // number: decimal value
+console.log("I sleep for about " + hours + " hours.");
 
 
 // Problem 3
 // Create a variable for your favorite fruit and print it in a full sentence.
-
+let fruit = mango; // string: text
+console.log("My favourite fruit is " + fruit);
 
 
 // Problem 4
 // Create two variables: one for your city and one for your country.
 // Print them on 2 separate lines.
+let city = Washington; // string: text
+console.log ("I live in the city of " + city );
 
-
+let country = USA; //string: text
+console.log ("I live in the country " + country);
 
 // Problem 5
 // Create 3 variables: your pet's name, its age, and its type.
 // Print them on the same line in a full sentence.
+
+// I don't have a pet but I will still make it up
+
+let name = Coco; //string: text
+let age = 4; //number: whole-number value
+let type = Labrador; //string: text
+console.log("My pet's name is " + name + ", and its age is " + age + " and it is a " + type );
