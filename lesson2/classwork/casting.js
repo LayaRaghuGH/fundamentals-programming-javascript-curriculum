013ce529bac413edce51b625da901b7a913992ac
LayaRaghuGH <layaraghu14@gmail.com>
2026-03-30 00:48:47 +0000

    finished casting lesson 2

---
+++ lesson2/classwork/casting.js
@@ -0,0 +1,11 @@
+let x = "42";
+let y = Number(x); // converts string to a number
+console.log(y * 3);
+
+let z = "3.14";
+let d = Number(z);
+console.log(d + 2.36); // converts string to a number
+
+let num = 100;
+let str = String(num); // converts number to a string
+console.log()
